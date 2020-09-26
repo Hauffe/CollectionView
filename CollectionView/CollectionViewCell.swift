4529371426116e833cff2ad590b3e4d2f4c82d06
Alexandre Hauffe <alex_hauffe@hotmail.com>
2020-09-26 11:38:40 -0300

    rows

---
+++ CollectionView/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Alex on 26/09/20.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+    
+}
